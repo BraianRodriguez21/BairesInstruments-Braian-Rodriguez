0a2068e3cc6dc7fdde5253df15c2868012b1a27e
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2023-11-06 17:33:32 -0300

    Commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// const USERNAME= prompt("Nombre de Usuario");
+// const PASSWORD= prompt("Contraseña");
+
+// if (USERNAME === "admin" &&(PASSWORD ==="secreta"|| PASSWORD === "123456789")){
+//     console.log("Iniciaste Sesion");
+// }else{
+//     console.log("No has podido iniciar sesion");
+// }
+
+
+
+// let numeroTotal = parseFloat(prompt("Ingresa Total"));
+// let numDescuento = parseFloat(prompt("Ingresa Descuento"));
+
+
+// function calculadoraConDescuento(total, descuento){
+//     let off= (total*descuento)/100;
+//     let totaloff= total - off;//
+    
+//     return totaloff;
+// }
+
+//     let totaloff = calculadoraConDescuento(numeroTotal,numDescuento);
+//     console.log("el total con el descuento es: "+ totaloff);
