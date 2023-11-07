19175f70e06e2d1e7f0f50bfaffc43ee83dae817
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2023-11-07 17:14:02 -0300

    cart attempt

--- js/main.js
+++ js/main.js
@@ -1,26 +1,70 @@
-//iniciar sesion
-const USERNAME= prompt("Nombre de Usuario");
-const PASSWORD= prompt("Contraseña");
+let password = "123456789";
 
-if (USERNAME === "admin" &&(PASSWORD ==="Secreta"|| PASSWORD === "123456789")){
-    console.log("Iniciaste Sesion");
-}else{
-    console.log("No has podido iniciar sesion");
-}
-
-
-
-//En caso de ofertas
-let PrecioTotal = parseFloat(prompt("Ingresa Total"));
-let PrecioOff = parseFloat(prompt("Ingresa Descuento"));
-
-
-function calculadoraConDescuento(total, descuento){
-    let off= (total*descuento)/100;
-    let totaloff= total - off;//
+const login = () => {
     
-    return totaloff;
+    let pass = false;
+
+    for (let tried = 3; tried > 0; tried--) {
+        let passwordPass = prompt("Ingresa tu contraseña. Tienes " + tried+  " intentos");
+
+        if (passwordPass === password) {
+            alert("Iniciaste Sesion");
+            pass = true;
+            break; 
+        } else {
+            alert("Contraseña inválida.");
+        }
+    }
+
+    return pass;
+    
 }
 
-    let totaloff = calculadoraConDescuento(PrecioTotal,PrecioOff);
-    console.log("el total con el descuento es: "+ totaloff);
+if (login())
+// intente hacer un carrito de compras
+{
+    class Producto {
+        constructor(nombre, precio) {
+            this.nombre  = nombre.toUpperCase();
+            this.precio  = parseFloat(precio);
+            this.vendido = false;
+        }
+        sumaIva() {
+            this.precio = this.precio * 1.21;
+        }
+        vender() {
+            this.vendido = true;
+        }
+    }
+    const producto1 = new Producto("Guitarra Criolla", 10000);
+    const producto2 = new Producto("Piano Yamaha", 700000);
+    const producto3 = new Producto("Auriculares Sure",40000);
+    const producto4 = new Producto("Microfono Sure",30000);
+    producto1.sumaIva();
+    producto2.sumaIva();
+    producto3.sumaIva();
+    producto4.sumaIva();
+    producto1.vender();
+    producto2.vender();
+    producto3.vender();
+
+    
+    
+}
+
+
+//En caso de ofertas o Cupon de descuento, esto lo dejo porque planeo poner cupos de descuento o para oferta. Lo dejo en comentarios porque me estoy fijando bien como usarlo
+
+// let PrecioTotal = parseFloat(prompt("Ingresa Total"));
+// let PrecioOff = parseFloat(prompt("Ingresa Descuento"));
+
+
+// function calculadoraConDescuento(total, descuento){
+//     let off= (total*descuento)/100;
+//     let totaloff= total - off;//
+    
+//     return totaloff;
+// }
+
+//     let totaloff = calculadoraConDescuento(PrecioTotal,PrecioOff);
+//     console.log("el total con el descuento es: "+ totaloff);
