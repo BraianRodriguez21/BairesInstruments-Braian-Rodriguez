5caf38353247dccd397394257cf0a234d495ea02
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2023-12-28 05:23:21 -0300

    Mejoras

--- js/main.js
+++ js/main.js
@@ -1,73 +1,56 @@
-//Preentrega 1
-
-
-let password = "123456789";
-
-const login = () => {
-    
-    let pass = false;
-
-    for (let tried = 3; tried > 0; tried--) {
-        let passwordPass = prompt("Ingresa tu contraseña. Tienes " + tried+  " intentos");
-
-        if (passwordPass === password) {
-            alert("Iniciaste Sesion");
-            pass = true;
-            break; 
+document.addEventListener("DOMContentLoaded", async function () {
+    try {
+        const username = await getUsername();
+        if (username === "admin") {
+            const password = await getPassword();
+            if (password === "Secreta" || password === "123456789") {
+                Swal.fire({
+                    title: "Inicio de sesión exitoso!",
+                    customClass: {
+                        container: 'custom-swal'
+                    }
+                });
+            } else {
+                Swal.fire({
+                    title: "Contraseña incorrecta. Inténtalo de nuevo.",
+                    customClass: {
+                        container: 'custom-swal'
+                    }
+                });
+            }
         } else {
-            alert("Contraseña inválida.");
+            Swal.fire({
+                title: "Nombre de usuario invalido.",
+                customClass: {
+                    container: 'custom-swal'
+                }
+            });
         }
+    } catch (error) {
+        console.error("Ocurrió un error:", error);
     }
+});
 
-    return pass;
-    
+async function getUsername() {
+    const { value } = await Swal.fire({
+        title: "Ingrese su nombre de usuario",
+        input: "text",
+        inputLabel: "Su Usuario",
+        inputPlaceholder: "Usuario"
+    });
+    return value;
 }
 
-if (login())
-// intente hacer un carrito de compras
-{
-    class Producto {
-        constructor(nombre, precio) {
-            this.nombre  = nombre.toUpperCase();
-            this.precio  = parseFloat(precio);
-            this.vendido = false;
+async function getPassword() {
+    const { value } = await Swal.fire({
+        title: "Ingresa tu contraseña",
+        input: "password",
+        inputLabel: "Contraseña",
+        inputPlaceholder: "Ingresa tu contraseña",
+        inputAttributes: {
+            maxlength: "10",
+            autocapitalize: "off",
+            autocorrect: "off"
         }
-        sumaIva() {
-            this.precio = this.precio * 1.21;
-        }
-        vender() {
-            this.vendido = true;
-        }
-    }
-    const producto1 = new Producto("Guitarra Criolla", 10000);
-    const producto2 = new Producto("Piano Yamaha", 700000);
-    const producto3 = new Producto("Auriculares Sure",40000);
-    const producto4 = new Producto("Microfono Sure",30000);
-    producto1.sumaIva();
-    producto2.sumaIva();
-    producto3.sumaIva();
-    producto4.sumaIva();
-    producto1.vender();
-    producto2.vender();
-    producto3.vender();
-
-    
-    
-}
-
-
-//En caso de ofertas o Cupon de descuento, esto lo dejo porque planeo poner cupos de descuento o para oferta. Lo dejo en comentarios porque me estoy fijando bien como usarlo
-
-// let PrecioTotal = parseFloat(prompt("Ingresa Total"));
-// let PrecioOff = parseFloat(prompt("Ingresa Descuento"));
-
-
-// function calculadoraConDescuento(total, descuento){
-//     let off= (total*descuento)/100;
-//     let totaloff= total - off;//
-    
-//     return totaloff;
-// }
-
-//     let totaloff = calculadoraConDescuento(PrecioTotal,PrecioOff);
-//     console.log("el total con el descuento es: "+ totaloff);
+    });
+    return value;}
